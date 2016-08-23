7d4bb4e2dc89b46fd546d6d298a180c783eab62b
Chaway <673262173@qq.com>
2016-08-23 16:35:22 +0800

    LPCS version 0.1.0

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
-#! /bin/sh
-make;
-filenames=`ls ./images/`
-for input_filename in $filenames
-do
-	echo $input_filename
-#	filename=${input_filename%.*}
-#        extension=${input_filename##*.}
-#	./segment $filename $extension
-	./segment $input_filename
-done
+ #! /bin/sh
+ make;
+ dir="./images/"
+ echo $dir
+ filenames=`ls $dir`
+ for input_filename in $filenames
+ do
+         echo ${dir}${input_filename}
+ #       filename=${input_filename%.*}
+ #        extension=${input_filename##*.}
+ #       ./segment $filename $extension
+        ./segment ${dir}${input_filename}                                             
+ done
